3ce58dd380f36be8c953a8335f883262108ee815
kimboseong <njunju77@nate.com>
2024-08-09 18:39:11 +0900

    추가: LoggingFilter 추가

---
+++ src/main/java/com/todolist/util/filter/LoggingFilter.java
@@ -0,0 +1,23 @@
+package com.todolist.util.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Slf4j(topic = "LoggingFilter")
+@Component
+@Order(1)
+public class LoggingFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        String url = request.getRequestURI();
+        log.info(url);
+        filterChain.doFilter(servletRequest, servletResponse);
+        log.info("Filter completed");
+    }
+}
