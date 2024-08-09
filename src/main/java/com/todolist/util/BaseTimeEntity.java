9d4fcdb028c686c0c50c82b67541c34c7967885f
kimboseong <njunju77@nate.com>
2024-08-09 13:33:34 +0900

    추가: BaseTimeEntity 생성

---
+++ src/main/java/com/todolist/util/BaseTimeEntity.java
@@ -0,0 +1,21 @@
+package com.todolist.util;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime createdAt;
+
+
+}
