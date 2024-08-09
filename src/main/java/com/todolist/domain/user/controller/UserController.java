e1ccf34156af388a1c877294a0e39a23537180aa
kimboseong <njunju77@nate.com>
2024-08-09 18:07:51 +0900

    수정: 컨트롤러 미작성부분 및 트랜잭션 추가

--- src/main/java/com/todolist/domain/user/controller/UserController.java
+++ src/main/java/com/todolist/domain/user/controller/UserController.java
@@ -2,6 +2,7 @@ package com.todolist.domain.user.controller;
 
 import com.todolist.domain.user.dto.SignUpDTO;
 import com.todolist.domain.user.dto.UserResponseDTO;
+import com.todolist.domain.user.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,8 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/users")
 public class UserController {
 
+    private final UserService userService;
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @PostMapping("/signup")
     public ResponseEntity<UserResponseDTO> signup(SignUpDTO request){
-        return ResponseEntity.status(HttpStatus.CREATED).body()
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.signup(request));
     }
 }
--- src/main/java/com/todolist/domain/user/service/UserServiceImpl.java
+++ src/main/java/com/todolist/domain/user/service/UserServiceImpl.java
@@ -5,6 +5,7 @@ import com.todolist.domain.user.dto.UserResponseDTO;
 import com.todolist.domain.user.model.User;
 import com.todolist.domain.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -13,6 +14,7 @@ public class UserServiceImpl implements UserService {
     public UserServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
+    @Transactional
     @Override
     public UserResponseDTO signup(SignUpDTO signUpDTO) {
         if (userRepository.existsByUsername(signUpDTO.getUsername())) {
