cac2aef461bfca852634f5ee2defe426fecea267
kimboseong <njunju77@nate.com>
2024-08-09 13:11:53 +0900

    init commit

---
+++ src/main/java/com/todolist/TodoListApplication.java
@@ -0,0 +1,15 @@
+package com.todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class TodoListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoListApplication.class, args);
+    }
+
+}
