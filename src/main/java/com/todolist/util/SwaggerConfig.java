ab11b92902096af70a446d1e1e676160135de84d
kimboseong <njunju77@nate.com>
2024-08-09 16:51:52 +0900

    추가: Swagger 의존성 및 config 파일

---
+++ src/main/java/com/todolist/util/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.todolist.util;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(
+                        new Info().title("Todolist API")
+                                .description("Todolist API schema")
+                                .version("1.0.0")
+                );
+    }
+}
