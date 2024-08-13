c4bc45fd94e2358ef31d182cdc93691e5bb4271a
kimboseong <njunju77@nate.com>
2024-08-13 17:34:33 +0900

    추가: swagger 토큰 넣어서 테스트할수있게 변경

--- src/main/java/com/todolist/util/Swagger/SwaggerConfig.java
+++ src/main/java/com/todolist/util/Swagger/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package com.todolist.util.Swagger;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +13,20 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .addSecurityItem(
+                        new SecurityRequirement().addList("Bearer Authentication")
+                )
+                .components(
+                        new Components().addSecuritySchemes(
+                                "Bearer Authentication",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .in(SecurityScheme.In.HEADER)
+                                        .name("Authorization")
+                        )
+                )
                 .info(
                         new Info().title("Todolist API")
                                 .description("Todolist API schema")
