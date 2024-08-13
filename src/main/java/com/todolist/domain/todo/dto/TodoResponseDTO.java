fb25a2dd2d3055a8eebf325bf36789e30e78285d
kimboseong <njunju77@nate.com>
2024-08-13 18:10:30 +0900

    수정: 일정 댓글까지 같이 반환

--- src/main/java/com/todolist/domain/todo/dto/TodoResponseDTO.java
+++ src/main/java/com/todolist/domain/todo/dto/TodoResponseDTO.java
@@ -1,11 +1,13 @@
 package com.todolist.domain.todo.dto;
 
+import com.todolist.domain.comment.dto.CommentResponseDTO;
 import com.todolist.domain.todo.model.Todo;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @NoArgsConstructor
@@ -16,15 +18,19 @@ public class TodoResponseDTO {
     private String content;
     private String writer;
     private LocalDateTime createdAt;
+    private List<CommentResponseDTO> commentList;
 
 
     public static TodoResponseDTO fromTodo(Todo todo) {
+        List<CommentResponseDTO> comments = todo.getComments().stream().map(CommentResponseDTO::from).toList();
+        // 엔티티 리스트 덩어리 dto 덩어리로 변환
         return new TodoResponseDTO(
                 todo.getId(),
                 todo.getTitle(),
                 todo.getContent(),
                 todo.getWriter(),
-                todo.getCreatedAt()
+                todo.getCreatedAt(),
+                comments
         );
     }
 }
