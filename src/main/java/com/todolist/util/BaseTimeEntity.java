1f87d31ae82560cd966f59499b9cd0cd9ed93a8c
kimboseong <njunju77@nate.com>
2024-08-14 11:57:26 +0900

    수정: BaseTimeEntity 업데이트 시간 항목 추가

--- src/main/java/com/todolist/util/BaseTimeEntity.java
+++ src/main/java/com/todolist/util/BaseTimeEntity.java
@@ -3,6 +3,7 @@ package com.todolist.util;
 import jakarta.persistence.*;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
@@ -17,5 +18,9 @@ public abstract class BaseTimeEntity {
     @Temporal(TemporalType.TIMESTAMP)
     private LocalDateTime createdAt;
 
+    @LastModifiedDate
+    @Column
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime updatedAt;
 
 }
