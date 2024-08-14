e5a0005a076923d8b6c7ee62f746e88e6958f2ac
kimboseong <njunju77@nate.com>
2024-08-14 11:44:55 +0900

    추가: error.log 생성하기 기능

--- src/main/java/com/todolist/exception/GlobalExceptionHandler.java
+++ src/main/java/com/todolist/exception/GlobalExceptionHandler.java
@@ -1,50 +1,69 @@
 package com.todolist.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponse> handleValidationExceptions(MethodArgumentNotValidException ex) {
 
-        StringBuilder errorMessage = new StringBuilder("유효성 검사 실패: ");
+        StringBuilder errorMessage = new StringBuilder("유효성 검사 실패, ");
 
 
         ex.getBindingResult().getAllErrors().forEach((error) -> {
             String fieldName = ((FieldError) error).getField();
             String message = error.getDefaultMessage();
             errorMessage.append(String.format("필드: %s, 오류: %s; ", fieldName, message));
+
         });
         // 오류 추출 + 메세지 생성
+        ErrorResponse errorResponse = new ErrorResponse(errorMessage.toString(),HttpStatus.BAD_REQUEST.value());
+        int status = HttpStatus.BAD_REQUEST.value();
+        log.error("상태코드: {}, 메세지: {}",status,errorMessage,ex);
+        // 로그파일에 남기기
 
-
-        return new ResponseEntity<>(
-                new ErrorResponse(errorMessage.toString(), HttpStatus.BAD_REQUEST.value()),
-                HttpStatus.BAD_REQUEST
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST
         );
     }
 
     @ExceptionHandler({IllegalArgumentException.class})
     public ResponseEntity<ErrorResponse> handleException(IllegalArgumentException ex) {
-        ErrorResponse ErrorResponse = new ErrorResponse(ex.getMessage(), HttpStatus.BAD_REQUEST.value());
-        return new ResponseEntity<>(ErrorResponse, HttpStatus.BAD_REQUEST);
+        ErrorResponse errorResponse = new ErrorResponse(ex.getMessage(), HttpStatus.BAD_REQUEST.value());
+
+        String errorMessage = errorResponse.getMessage();
+        int status = errorResponse.getStatusCode() ;
+
+        log.error("상태코드: {}, 메세지: {}",status,errorMessage,ex);
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ErrorResponse> handleNotFoundExceptions(NotFoundException ex) {
-        ErrorResponse ErrorResponse = new ErrorResponse(ex.getMessage(),HttpStatus.NOT_FOUND.value() );
-        return new ResponseEntity<>(ErrorResponse, HttpStatus.NOT_FOUND);
+        ErrorResponse errorResponse = new ErrorResponse(ex.getMessage(),HttpStatus.NOT_FOUND.value() );
 
+        String errorMessage = errorResponse.getMessage();
+        int status = errorResponse.getStatusCode() ;
+
+        log.error("상태코드: {}, 메세지: {}",status,errorMessage,ex);
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(UserException.class)
     public ResponseEntity<ErrorResponse> handleUserExceptions(UserException ex) {
-        ErrorResponse ErrorResponse = new ErrorResponse(ex.getMessage(), HttpStatus.BAD_REQUEST.value());
-        return new ResponseEntity<>(ErrorResponse, HttpStatus.BAD_REQUEST);
+        ErrorResponse errorResponse = new ErrorResponse(ex.getMessage(), HttpStatus.BAD_REQUEST.value());
+
+        String errorMessage = errorResponse.getMessage();
+        int status = errorResponse.getStatusCode() ;
+
+        log.error("상태코드: {}, 메세지: {}",status,errorMessage,ex);
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 }
